fd3d387e80449266e0d81e47dc1bef40adf9a62a
gabriellecalise <gcalise@ufl.edu>
2018-02-19 15:24:34 -0500

    deleted credentials.py
    goodbye sweet friend

--- credentials.py
+++
@@ -1,6 +0,0 @@
-#This file hides things in GitIgnore so that people on the web who are lookings at our files will not see it and steal our stuff and tweet inappropriate things and MINE BITCOIN (etc.)
-
-
-#variables for keys and such
-
-#hello world
