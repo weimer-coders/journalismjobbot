a91d6730fef25ee706d0a757c876297c93614f34
gabriellecalise <gcalise@ufl.edu>
2018-02-19 18:51:59 -0500

    fixed a mistake i made in labeling the variable in twitter.py

--- twitter.py
+++ twitter.py
@@ -6,7 +6,7 @@ import os
 #config variables from Heroku
 consumer_key = os.environ.get('consumer_key')
 consumer_secret = os.environ.get('consumer_secret')
-access_secret = os.environ.get('access_secret')
+access_token = os.environ.get('access_token')
 access_token_secret = os.environ.get('access_token_secret')
 
 
