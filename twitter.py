342a41ff6d0622dc604bf39d052c755e0b8a4792
gabriellecalise <gcalise@ufl.edu>
2018-02-19 15:23:23 -0500

    updated twitter.py to pull from secrets.py

--- twitter.py
+++ twitter.py
@@ -1,6 +1,6 @@
 import tweepy
 from time import sleep
-from credentials import *
+from secrets import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
